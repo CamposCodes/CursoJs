c673b5d41f862947d8829479ca4975e3e3ba67f4
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-08-15 17:57:58 -0300

    atualiza aulas

--- aula40.js
+++ aula40.js
@@ -1,2 +1,21 @@
 /* AULA 40 - MONTADOR DE ENDEREÇOS - MINI PROJETO */
 
+// Objeto endereço
+
+// Criar um objeto endereço que contem:
+// Rua - Cidade - CEP - Exibir endereço
+
+let endereco = {
+    rua: 'x',
+    cidade: 'xx',
+    cep: 'xxx'
+};
+
+function exibirEndereco(endereco){
+    for(let chave in endereco)
+    {
+        console.log(chave,endereco[chave]);
+    }
+}
+
+exibirEndereco();
